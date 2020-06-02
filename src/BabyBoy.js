1913ac8c2aa7ff838150322cfde080c447b07697
SERKAN CAVUS <40465585+SCavus@users.noreply.github.com>
2020-06-02 02:05:11 +0100

    Level 1-3

---
+++ src/BabyBoy.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const BabyBoy = () => {
+    return (
+        <img src="" alt="boy"/>
+    )
+}
--- src/ListBabyNames.js
+++ src/ListBabyNames.js
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+//import data from "./babyNamesData.json";
 
 const ListBabyNames = (props) => {
+  const [selectName, setSelectName] = useState([]);
+  const [removedName, setRemovedNAme] = useState(props.babyNames);
+  console.log(removedName);
+
+  const addToFavorites = (value) => {
+    setRemovedNAme(removedName.filter((item) => item.id !== value.id));
+    setSelectName((selectName) => [...selectName, value]);
+  };
+  
+  const removeFromFavorites = (value) => {
+    setRemovedNAme((removedName) => [...removedName, value]);
+    setSelectName(selectName.filter((item) => item.id !== value.id));
+  }
+
   return (
-    <div className='container'>
-      <img
-        src="yin-yang.png"
-        alt="yin-yang"
-      />
-      <div className="all-baby-names">
-        {props.babyNames
-          .sort((a, b) => (a.name > b.name ? 1 : -1))
-          .map((baby, index) => (
-            <p key={index} className={baby.sex}>
-              {baby.name}
-            </p>
+    <div>
+      <div className="container">
+        <img src="yin-yang.png" alt="yin-yang" />
+        <div className="favorites">
+          <span>Favourites: </span>
+          {selectName.map((name) => (
+            <span className={name.sex} onClick={()=>removeFromFavorites(name)}>{name.name}</span>
           ))}
+        </div>
+        <div className="all-baby-names">
+          {removedName
+            .sort((a, b) => (a.name > b.name ? 1 : -1))
+            .map((baby, index) => (
+              <p
+                key={index}
+                className={baby.sex}
+                onClick={() => addToFavorites(baby)}
+              >
+                {baby.name}
+              </p>
+            ))}
+        </div>
+        <hr color="red" />
       </div>
-      <hr color="red" />
     </div>
   );
 };
