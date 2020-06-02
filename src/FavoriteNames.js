2001583f97b6e7fb8c73b9b7383096baca5a8023
SERKAN CAVUS <40465585+SCavus@users.noreply.github.com>
2020-06-02 20:21:40 +0100

    sound effect

--- src/FavoriteNames.js
+++ src/FavoriteNames.js
@@ -1,17 +1,38 @@
 import React from "react";
-import useSound from 'use-sound';
+import useSound from "use-sound";
+import mozart from "./mozart.mp3";
 
-const FavoriteNames = (props) => { 
+const FavoriteNames = (props) => {
+  // const [play] = useSound(mozart)
+  const [play, { stop }] = useSound(mozart, { volume: 0.5 });
+
+  const [isHovering, setIsHovering] = React.useState(false);
+
   return (
     <div>
       <div className="container">
-        <img src="yin-yang.png" alt="yin-yang" >
-
-        </img>
+        <img
+          onMouseEnter={() => {
+            setIsHovering(true);
+            play();
+          }}
+          onMouseLeave={() => {
+            setIsHovering(false);
+            stop();
+          }}
+          src="yin-yang.png"
+          alt="yin-yang"
+        />
         <div className="favorites">
           <span>Favourites: </span>
           {props.names.map((name) => (
-            <span key={name.id} className={name.sex} onClick={()=>props.remove(name)}>{name.name}</span>
+            <span
+              key={name.id}
+              className={name.sex}
+              onClick={() => props.remove(name)}
+            >
+              {name.name}
+            </span>
           ))}
         </div>
       </div>
